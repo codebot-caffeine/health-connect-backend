6d5e883332083f955dffd0cc2627b6b2a32bf6bd
krishnatejab <krishnatejab@turito.com>
2023-07-06 12:20:23 +0530

    error message in auth is formatted

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,13 +7,13 @@ const verifyToken = (req, res, next) => {
     req.body.token || req.query.token || req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    return res.status(403).send({status:false,errorMessage:"A token is required for authentication"});
   }
   try {
     const decoded = jwt.verify(token, TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).send({status:false,errorMessage:"Invalid Token"});
   }
   return next();
 };
